63217fd9a1634e2e08d687c2a3e9dccc058af5c4
Stephen Fraser <stephen.fraser@digirati.com>
2022-08-25 16:14:12 +0100

    Small fixes

--- src/themes/example-theme.ts
+++ src/themes/example-theme.ts
@@ -1,21 +1,26 @@
-import { BaseTheme } from '@madoc.io/types/dist/types/schemas/madoc-theme';
+import { MadocTheme } from "@madoc.io/types/dist/frontend/themes/definitions/types";
 
-export const exampleTheme: BaseTheme & { id: string } = {
+const theme: MadocTheme = {
+  header: 'default',
+  global: 'default',
+  accent: 'default',
+  footer: 'dark',
+  siteContainer: 'dark',
+  custom: {
+    header: {
+      headerBackground: 'blue'
+    },
+    global: {
+      maxWidth: '1200px',
+    },
+  },
+};
+
+export const exampleTheme = {
   id: 'madoc-plugin-theme',
   name: 'Madoc plugin theme',
   version: '1.0.0',
   description: 'This is my theme loaded from a plugin',
   thumbnail: 'https://user-images.githubusercontent.com/8266711/118507448-25750b80-b726-11eb-9f87-d96f43c0a894.png',
-  theme: {
-    header: 'default',
-    global: 'default',
-    accent: 'default',
-    footer: 'dark',
-    siteContainer: 'dark',
-    custom: {
-      global: {
-        maxWidth: '1200px',
-      },
-    },
-  },
+  theme,
 };
